536821d5be604e73c4035f3ca29369cbc5203c7c
ezhilra <ezhil.dev@gmail.com>
2024-12-27 14:57:38 +0530

    Commited

---
+++ user-inputs-If-Else.py
@@ -0,0 +1,8 @@
+perc = int(input("Enter your percentage: "))
+if 70 < perc < 100:
+    name = str(input("Enter your name: "))
+    dept = str(input("Enter your department: "))
+    loc = str(input("Enter your location: "))
+    print("You are eligible")
+else:
+    print("You are not eligible")
