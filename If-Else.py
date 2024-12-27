f398c59f1bc498404f147125815922e8bbc672de
ezhilra <ezhil.dev@gmail.com>
2024-12-27 13:30:04 +0530

    Commited

--- If-Else.py
+++ If-Else.py
@@ -1,11 +1,11 @@
-mark = int(input("Please provide the mark\n"))
+mark = int(input("Please provide the mark:\n"))
 if mark > 35:
     print("Pass")
 else:
     print("Fail")
 
 #Next example
-income = int(input("Please key in the income\n"))
+income = int(input("Please key in the income:\n"))
 if income > 7000:
     print("Ineligible for Scholarship")
 else:
