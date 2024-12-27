0b2bb2a1a94ebc1eb5342854021355bab18d9923
ezhilra <ezhil.dev@gmail.com>
2024-12-27 16:31:09 +0530

    Commited

--- user-inputs-If-Else.py
+++ user-inputs-If-Else.py
@@ -1,10 +1,11 @@
-#Next example : Loan eligibility check based on age and salary
+#Next example : Loan eligibility check based on age and salary - Nested if else
 sal = int(input("Enter your salary: "))
 age = int(input("Enter your age: "))
 if sal >= 20000 or age <=25:
+    print("You are eligible for the loan")
     loan_amt = int(input("Enter the loan amount: "))
     if loan_amt <= 50000:
-        print("You are eligible for the loan")
+        print("Loan approved")
     else:
         print("Maximum loan amount is 50000")
 else:
