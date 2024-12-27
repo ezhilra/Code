6381ee90394902266eaeb9ceca7182473b8ebbdc
ezhilra <ezhil.dev@gmail.com>
2024-12-27 11:00:11 +0530

    Commited

---
+++ Laadu.py
@@ -0,0 +1,3 @@
+myList = [1,2,3,4,5,6,7,8,9,10]
+del myList[::2]
+print(myList)
---
+++ Strings.py
@@ -0,0 +1,9 @@
+# strings and texts cannot be combined. But using Fstring they can be combined
+award_count = 5
+strings = f"The movie 300 had won {award_count} academy awards"
+print(strings)
+
+# placeholders and modifiers
+# A placeholder can contain variables, operations, functions, and modifiers to format the value
+
+
