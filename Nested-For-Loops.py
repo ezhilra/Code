5117e35db4c6226e114d18b5fe8b258387e97b12
ezhilra <ezhil.dev@gmail.com>
2024-12-31 19:04:17 +0530

    Commited

---
+++ Nested-For-Loops.py
@@ -0,0 +1,50 @@
+# Example 1 : Nested For Loop
+for i in range(1, 6):
+    for j in range(1, 4):
+        print(i, j)
+"""
+# Example 2 : Nested For Loop with if condition
+for i in range(1, 6):
+    for j in range(1, 6):
+        if i == j:
+            print(i, j)
+            
+# Example 3 : Nested For Loop with if condition and break
+for i in range(1, 6):
+    for j in range(1, 6):
+        if i == j:
+            print(i, j)
+            break
+            
+# Example 4 : Nested For Loop with if condition and continue
+for i in range(1, 6):
+    for j in range(1, 6):
+        if i == j:
+            print(i, j)
+            continue
+            
+# Example 5 : Nested For Loop with if condition and pass
+for i in range(1, 6):
+    for j in range(1, 6):
+        if i == j:
+            print(i, j)
+            pass
+        
+# Example 6 : Nested For Loop with if condition and pass
+for i in range(1, 6):
+    for j in range(1, 6):
+        if i == j:
+            print(i, j)
+            pass
+    print("End of Inner Loop")
+print("End of Outer Loop")
+
+# Example 7 : Nested For Loop with if condition and pass
+for i in range(1, 6):
+    for j in range(1, 6):
+        if i == j:
+            print(i, j)
+            pass
+        print("End of Inner Loop")
+print("End of Outer Loop")
+"""
