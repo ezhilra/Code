9b5eb75ce7430fd1a4f928a44fd6689c4639a7c2
ezhilra <ezhil.dev@gmail.com>
2025-01-01 17:31:49 +0530

    Commited

--- Nested-For-Loops.py
+++ Nested-For-Loops.py
@@ -1,7 +1,8 @@
 # Example 1 : Nested For Loop
-for i in range(1, 6):
-    for j in range(1, 4):
-        print(i, j)
+for i in range(1, 5):
+    for j in range(i):
+        print("*",end="")
+    print()
 """
 # Example 2 : Nested For Loop with if condition
 for i in range(1, 6):
