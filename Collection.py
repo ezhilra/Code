e3e28df56380ebb1ef2dd01accce02f5540cd329
ezhilra <ezhil.dev@gmail.com>
2025-03-13 21:14:17 +0530

    Commited

---
+++ Collection.py
@@ -0,0 +1,12 @@
+# List : Ordered, mutable, allows duplicate elements
+# Tuple : Ordered, immutable, allows duplicate elements
+# Set : Unordered, mutable, does not allow duplicate elements
+# Dictionary : Unordered, mutable, indexed, no duplicate elements
+
+# List example
+a = [1,2,3,4,5,6,7,8,9,10,11,12]
+print(a)
+a.append(10)
+a.append(True)
+a.append(6)
+print(a)
